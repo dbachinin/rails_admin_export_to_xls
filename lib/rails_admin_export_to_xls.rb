8db1804ba838d700169444e63e1bcb643272306f
Dmitry Bachinin <dbachinin@gmail.com>
2023-07-25 16:23:09 +0200

    init

---
+++ lib/rails_admin_export_to_xls.rb
@@ -0,0 +1,57 @@
+require "rails_admin_export_to_xls/engine"
+require "rails_admin_export_to_xls/xls_converter"
+
+module RailsAdminExportToXls
+end
+
+require 'rails_admin/config/actions'
+
+module RailsAdmin
+  module Config
+    module Actions
+      class ExportToXls < Base
+        RailsAdmin::Config::Actions.register(self)
+        register_instance_option :object_level do
+          true
+        end
+
+        register_instance_option :collection do
+          true
+        end
+
+        register_instance_option :http_methods do
+          [:get, :post]
+        end
+
+        register_instance_option :bulkable? do
+          true
+        end
+
+        register_instance_option :link_icon do
+          'icon-download'
+        end
+
+        register_instance_option :controller do
+          proc do
+            if format = params[:xls]
+              request.format = format
+              @objects = list_entries(@model_config, :export)
+              @schema = HashHelper.symbolize(params[:schema]) if params[:schema]
+              header, encoding, output = RailsAdminExportToXls::XLSConverter.new(@objects, @schema).to_xls(params[:csv_options])
+              if params[:send_data]
+                send_data output,
+                  type: "application/vnd.ms-excel; charset=#{encoding}; #{'header=present' if header}",
+                  disposition: "attachment; filename=#{params[:model_name]}_#{DateTime.now.strftime('%Y-%m-%d_%Hh%Mm%S')}.xls"
+              else
+                render text: output
+              end
+            else
+              render @action.template_name
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
---
+++ rails_admin_export_to_xls.gemspec
@@ -0,0 +1,20 @@
+$:.push File.expand_path("../lib", __FILE__)
+
+# Maintain your gem's version:
+require "rails_admin_export_to_xls/version"
+
+# Describe your gem and declare its dependencies:
+Gem::Specification.new do |s|
+  s.name        = "rails_admin_export_to_xls"
+  s.version     = RailsAdminExportToXls::VERSION
+  s.authors     = ["Dmitry Bachinin"]
+  s.email       = ["dbachinin@gmail.com"]
+  s.homepage    = "TODO"
+  s.summary     = "Summary of RailsAdminExportToXls."
+  s.description = "Description of RailsAdminExportToXls."
+  s.license     = "MIT"
+
+  s.files = Dir["{app,config,db,lib}/**/*", "MIT-LICENSE", "Rakefile", "README.rdoc"]
+
+  s.add_dependency "rails", "~> 6.1.7"
+end
